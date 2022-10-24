42f52c69f807027f8a3dc364912bbc9efb43f0fe
supermanisme2002 <bthanh2001@gmail.com>
2022-10-25 00:37:10 +0700

    ts

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log('Hello TypeScript!');
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+    apple: a,
+    banana: b
+};
+let d = c.apple * 4;
+console.log(d);
+console.log("co con c");
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+let oneMillion = 1_000_000 // Equivalent to 1000000
+let twoMillion: 2_000_000 = 2_000_000 //explicitly that your value is a specific number 2000000
